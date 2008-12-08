e0d586ee9afbb8df5f8f4505e55fcaf600db53d3
Ignacio Casal Quinteiro <nacho.resa@gmail.com>
2008-12-08 12:59:37 +0100

    2008-12-08  Ignacio Casal Quinteiro  <nacho.resa@gmail.com>
            * gtksourcecompletion/gsc-popup.c:
            * gtksourcecompletion/gsc-popup.h:
            Use the right coding style.

--- gtksourcecompletion/gsc-popup.h
+++ gtksourcecompletion/gsc-popup.h
@@ -25,6 +25,9 @@
 
 G_BEGIN_DECLS
 
+/*
+ * FIXME: Should be called something like GscPopupKeysType
+ */
 typedef enum {
 	KEYS_INFO,
 	KEYS_PAGE_NEXT,
@@ -48,226 +51,72 @@ typedef struct _GscPopupPriv GscPopupPriv;
 typedef struct _GscPopup GscPopup;
 typedef struct _GscPopupClass GscPopupClass;
 
+struct _GscPopup
+{
+	GtkWindow parent;
+
+	GscPopupPriv *priv;
+};
+
 struct _GscPopupClass
 {
 	GtkWindowClass parent_class;
-  
+
 	void     (* proposal_selected)(GscPopup *popup, GscProposal *proposal);
 	gboolean (*display_info)      (GscPopup *popup, GscProposal *proposal);
 };
 
-struct _GscPopup
-{
-  GtkWindow parent;
-  
-  GscPopupPriv *priv;
+GType		 gsc_popup_get_type			(void) G_GNUC_CONST;
 
-};
+GtkWidget	*gsc_popup_new				(void);
 
-GType gsc_popup_get_type (void) G_GNUC_CONST;
+void		 gsc_popup_add_data			(GscPopup *self,
+							 GscProposal *data);
 
-/**
- * gsc_popup_select_first:
- * @self: The #GscPopup
- *
- * See #gsc_tree_select_first
- *
- * Returns
- */
-gboolean
-gsc_popup_select_first(GscPopup *self);
+void		 gsc_popup_clear			(GscPopup *self);
 
-/**
- * gsc_popup_select_last:
- * @self: The #GscPopup
- *
- * See #gsc_tree_select_last
- *
- * Returns
- */
-gboolean 
-gsc_popup_select_last(GscPopup *self);
+gboolean	 gsc_popup_select_first			(GscPopup *self);
 
-/**
- * gsc_popup_select_previous:
- * @self: The #GscPopup
- *
- * See #gsc_tree_select_previous
- *
- * Returns
- */
-gboolean
-gsc_popup_select_previous(GscPopup *self, 
-			  gint rows);
+gboolean	 gsc_popup_select_last			(GscPopup *self);
 
-/**
- * gsc_popup_select_next:
- * @self: The #GscPopup
- *
- * See #gsc_tree_select_next
- *
- * Returns
- */
-gboolean
-gsc_popup_select_next(GscPopup *self, 
-		      gint rows);
+gboolean	 gsc_popup_select_previous		(GscPopup *self, 
+							 gint rows);
 
-/**
- * gsc_popup_get_selected_proposal:
- * @self: The #GscPopup
- *
- * See #gsc_tree_get_selected_proposal. Not free the proposal!
- *
- * Returns
- */
-gboolean
-gsc_popup_get_selected_proposal(GscPopup *self,
-				GscProposal **proposal);
+gboolean	 gsc_popup_select_next			(GscPopup *self, 
+							 gint rows);
 
-/**
- * gsc_popup_new:
- *
- * Returns The new #GscPopup
- */
-GtkWidget*
-gsc_popup_new ();
+gboolean	 gsc_popup_get_selected_proposal	(GscPopup *self,
+							 GscProposal **proposal);
 
-/**
- * gsc_popup_clear:
- * @self: The #GscPopup
- *
- * Clear all proposals in all pages. It frees all completion proposals.
- *
- */
-void
-gsc_popup_clear(GscPopup *self);
+gboolean	 gsc_popup_has_proposals		(GscPopup *self);
 
-/**
- * gsc_popup_add_data:
- * @self: The #GscPopup
- * @data: The #GscProposal to add.
- *
- * The popup frees the proposal when it will be cleaned.
- *
- */
-void
-gsc_popup_add_data(GscPopup *self,
-		   GscProposal* data);
+void		 gsc_popup_toggle_proposal_info		(GscPopup *self);
 
-/**
- * gsc_popup_has_proposals:
- * @self: The #GscPopup
- *
- * Returns TRUE if the popup has almost one element.
- */
-gboolean
-gsc_popup_has_proposals(GscPopup *self);
+void		 gsc_popup_page_next			(GscPopup *self);
 
-/**
- * gsc_popup_toggle_proposal_info:
- * @self: The #GscPopup
- *
- * This toggle the state of the info dialog. If the info is visible
- * then it hide the info dialog. If the dialog is hidden then it 
- * shows the info dialog.
- *
- */
-void
-gsc_popup_toggle_proposal_info(GscPopup *self);
+void		 gsc_popup_page_previous		(GscPopup *self);
 
-/**
- * gsc_popup_page_next:
- * @self: The #GscPopup
- *
- * Shows the next completion page. If it is the last page then 
- * shows the first one.
- *
- */
-void
-gsc_popup_page_next(GscPopup *self);
+void		 gsc_popup_set_current_info		(GscPopup *self,
+							 const gchar *info);
 
-/**
- * gsc_popup_page_previous:
- * @self: The #GscPopup
- *
- * Shows the previous completion page. If it is the first page then
- * shows the last one.
- *
- */
-void
-gsc_popup_page_previous(GscPopup *self);
+gint		 gsc_popup_get_num_active_pages		(GscPopup *self);
 
-/**
- * gsc_popup_set_current_info:
- * @self: The #GscPopup
- * @info: Markup with the info to be shown into the info window
- */
-void
-gsc_popup_set_current_info(GscPopup *self,
-			   gchar *info);
+void		 gsc_popup_set_key			(GscPopup *self,
+							 KeysType type,
+							 const gchar* keys);
 
-/**
- * gsc_popup_get_num_active_pages:
- * @self: The #GscPopup
- *
- * Returns The number of active pages (pages with proposals)
- */
-gint
-gsc_popup_get_num_active_pages(GscPopup *self);
+gchar 		*gsc_popup_get_key			(GscPopup *self,
+							 KeysType type);
 
-/**
- * gsc_popup_set_key:
- * @self: The #GscPopup
- * @type: Key what you want to set
- * @keys: Keys to be assigned to the key type
- *
- * Sets the keys for the key type. By example you can set the info keys 
- * by passing KEYS_INFO type and "<control>i" keys.
- *
- */
-void
-gsc_popup_set_key(GscPopup *self, KeysType type, const gchar* keys);
+gboolean	 gsc_popup_manage_key			(GscPopup *self,
+							 GdkEventKey *event);
 
-/**
- * gsc_popup_get_key:
- * @self: The #GscPopup
- * @type: The key type what you want to get
- *
- * Returns: New allocated string representation with #gtk_accelerator_name
- */
-gchar*
-gsc_popup_get_key(GscPopup *self, KeysType type);
+void		 gsc_popup_show_or_update		(GtkWidget *widget);
 
-/**
- * gsc_popup_manage_key:
- * @self: The #GscPopup
- * @event: Key event to be managed
- *
- * Manage the event keys. If it is Return, it will select the current proposal, 
- * if it is a Down key then selects the next proposal etc.
- *
- * Returns: TRUE if the key has been used.
- */
-gboolean
-gsc_popup_manage_key(GscPopup *self,
-		     GdkEventKey *event);
+void		 gsc_popup_bottom_bar_set_visible	(GscPopup *self,
+							 gboolean visible);
 
-/**
- * gsc_popup_show_or_update:
- * @widget: The #GscPopup
- *
- * Show the completion popup or update if it is visible because 
- * using gtk_widget_show only works if the popup is not visible.
- *
- */
-void
-gsc_popup_show_or_update(GtkWidget *widget);
-
-void
-gsc_popup_bottom_bar_set_visible(GscPopup *self, gboolean visible);
-
-gboolean
-gsc_popup_bottom_bar_get_visible(GscPopup *self);
+gboolean	 gsc_popup_bottom_bar_get_visible	(GscPopup *self);
 			   
 G_END_DECLS
 
