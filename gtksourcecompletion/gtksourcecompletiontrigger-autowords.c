d204f11c1e20e39d4488f373008bee7cf24157e0
Simisani Takobana <chuchiperriman@gmail.com>
2009-03-20 16:04:42 +0100

    Changed the license from LGPL to GPL

--- gtksourcecompletion/gtksourcecompletiontrigger-autowords.c
+++ gtksourcecompletion/gtksourcecompletiontrigger-autowords.c
@@ -1,19 +1,23 @@
-/* Copyright (C) 2007 - Jesús Barbero <chuchiperriman@gmail.com>
- * 
- *  This file is part of gtksourcecompletion.
+/*
+ * gtksourcecompletiontrigger-autowords.c
+ * This file is part of gtksourcecompletion
  *
- *  gtksourcecompletion is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Copyright (C) 2007 -2009 Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
  *
- *  gtksourcecompletion is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307, USA.
  */
 
 /**
